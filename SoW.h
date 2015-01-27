cfa73e2a1cb0871de2b0c1876ec09a847aa6d3bb
arjandha <arjandhaliwal@hotmail.com>
2015-01-27 12:53:06 -0800

    Added sdcard functions and serial port write

--- SoW.h
+++ SoW.h
@@ -4,8 +4,11 @@
 #include <altera_up_ps2_keyboard.h>
 #include "alt_types.h"
 #include <sys/alt_irq.h>
-
+#include <altera_up_sd_card_avalon_interface.h>
+#include "altera_up_avalon_rs232.h"
 #include <stdio.h>
+#include "io.h"
+#include <stdlib.h>
 
 
 /* IRQ ID */
@@ -19,7 +22,7 @@
 #define SERIAL_DATA_LOC (alt_u8 *) 0x0
 #define SERIAL_PAR_LOC (alt_u8 *) 0x4
 
-#define SERIAL_BASE (volatile int *) 0x4070;
+#define SERIAL_BASE (volatile int *) 0x4070
 #define KEY_BASE 0x4078
 
 char *KeyInput;
@@ -34,6 +37,14 @@ void keyboard_read(void);
 
 void audio_init(void);
 void audio_play(void);
-// void serial_init(void); 
+
+void serial_init(void);
+void serial_write(alt_u8);
+
+void sdcard_init(void);
+int sdcard_write_file(char*, int*, int);
+int sdcard_read_file(char*, int*, int);
+
+
 
 
---
+++ SoW_sdcard.c
@@ -0,0 +1,53 @@
+/*
+ * SoW_sdcard.c
+ *
+ *  Created on: 2015-01-27
+ *      Author: Arjan
+ */
+
+
+#include "SoW.h"
+
+alt_up_sd_card_dev* sdcard;
+
+void sdcard_init() {
+	sdcard = alt_up_sd_card_open_dev("/dev/sdcard");
+}
+
+int sdcard_write_file(char* file_name, int* buffer, int size) {
+	if(alt_up_sd_card_is_Present() && alt_up_sd_card_is_FAT16() && sdcard != NULL) {
+		int fd = alt_up_sd_card_fopen(file_name, true);
+		int i = 0;
+		int temp = 0;
+		while(i < size && temp >= 0) {
+			temp = buffer[i];
+			alt_up_sd_card_write(fd, buffer[i]);
+			i++;
+		}
+		alt_up_sd_card_fclose(fd);
+	}
+	else {
+		return -1;
+	}
+	return 1;
+}
+
+int sdcard_read_file(char* file_name, int* buffer, int size) {
+	if(alt_up_sd_card_is_Present() && alt_up_sd_card_is_FAT16() && sdcard != NULL) {
+		int fd = alt_up_sd_card_fopen(file_name, false);
+		int i = 0;
+		int temp = 0;
+		while(i < size && temp >= 0) {
+			temp = alt_up_sd_card_read(fd);
+			buffer[i] = temp;
+			printf("%d, %d\n", i, temp);
+			i++;
+		}
+		alt_up_sd_card_fclose(fd);
+	}
+	else {
+		return -1;
+	}
+	return 1;
+}
+
