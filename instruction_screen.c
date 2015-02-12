4b255b49628c41be605d8c09739786c0245518c5
Pamela Anderson <pamviolet.a@gmail.com>
2015-02-11 19:09:59 -0800

    Fix broken changes

--- instruction_screen.c
+++ instruction_screen.c
@@ -7,16 +7,18 @@
  * @brief Clear screen and display game play instructions.
  */
 void show_instructions() {
+	keypress key;
 	alt_up_pixel_buffer_dma_clear_screen(pixel_buffer, 0);
 	alt_up_char_buffer_clear(char_buffer);
 	alt_up_char_buffer_string(char_buffer, "Spirits of War", 30, 10);
 	alt_up_char_buffer_string(char_buffer, "How to play", 32, 15);
 	alt_up_char_buffer_string(char_buffer, "Kill everyone! Text text text text text text text text text text text.", 0, 20);
 	alt_up_char_buffer_string(char_buffer, "Kill everyone! Text text text text text text text text text text text.", 0, 22);
-	while(alt_up_rs232_read_data(serial_port, SERIAL_DATA_LOC, SERIAL_PAR_LOC) == -1){}
-		if(*SERIAL_DATA_LOC == ' ') {
-            return;
+	while(1) {
+		key = get_player_input(0);
+		if((key == ENTER) || (key == ESC)) {
+			return;
 		}
-	printf("data: %c\n", *SERIAL_DATA_LOC);
+	}
 }
 
--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include "SoW.h"
 #include <stdio.h>
 
-int main(void)
-{
+int main(void) {
 	music_load("ee3.wav");
 	audio_init();
 	music_enable_ISR();
 	keyboard_init();
-	keyboard_enable_ISR();
+	show_menu();
 }
