ce03d8ddfc0683d558afe02e56caa260c690c8a2
Kryostat <e.kevin.xu@gmail.com>
2015-02-11 20:21:37 -0800

    Update instruction_screen.c

--- instruction_screen.c
+++ instruction_screen.c
@@ -12,8 +12,10 @@ void show_instructions() {
 	alt_up_char_buffer_clear(char_buffer);
 	alt_up_char_buffer_string(char_buffer, "Spirits of War", 30, 10);
 	alt_up_char_buffer_string(char_buffer, "How to play", 32, 15);
-	alt_up_char_buffer_string(char_buffer, "Kill everyone! Text text text text text text text text text text text.", 0, 20);
-	alt_up_char_buffer_string(char_buffer, "Kill everyone! Text text text text text text text text text text text.", 0, 22);
+	alt_up_char_buffer_string(char_buffer, "Your goal is to eliminate the enemy team!", 0, 20);
+	alt_up_char_buffer_string(char_buffer, "Navigate the on-screen prompts with 'W' for Up, 'S' for Down, 'A' For Left, 'D' for Right and '/' to confirm!", 0, 22);
+	alt_up_char_buffer_string(char_buffer, "To exit at any time press '\'", 0, 24);
+	alt_up_char_buffer_string(char_buffer, "Good Luck!", 0, 28);
 	while(1) {
 		key = get_player_input(0);
 		if((key == ENTER) || (key == ESC)) {
